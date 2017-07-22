9549d0bf2c8557018a829c096df82857d2db0b38
Petar Metodiev <petar.metodiev@gmail.com>
2017-07-22 11:21:12 +0300

    Color point example
    Convert Int to Float with fromIntegral

--- tuples.hs
+++ tuples.hs
@@ -1,3 +1,4 @@
+-- Point example
 type Point = (Float, Float)
 
 origin :: Point
@@ -14,3 +15,20 @@ squareTwo a b = a*a + b*b
 
 calculateDistance :: Point -> Float
 calculateDistance (x, y) = sqrt (squareTwo x y) - fst origin
+
+-- Color point example
+type Color = String
+type ColorPoint = (Int, Int, Color)
+
+colorOrigin :: Color -> ColorPoint
+colorOrigin color = (0,0,color)
+
+colorMove :: ColorPoint -> Int -> Int -> ColorPoint
+colorMove (x,y,color) dx dy = (x + dx, y + dy, color)
+
+colorDistance :: ColorPoint -> ColorPoint -> Float
+colorDistance (x1,y1,color1) (x2,y2,color2)
+  = sqrt (fromIntegral (dx * dx + dy * dy))
+  where
+   dx = abs x1 - x2
+   dy = abs y1 - y2
